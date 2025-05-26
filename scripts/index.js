16c4d3644e5302e3a37acdbd13e9fd2fd5cb20f2
Sergey Grischyov <sergey@shadeapps.com>
2025-05-26 14:47:10 +0100

    Add cardTemplate and placesList

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
 // @todo: DOM узлы
 

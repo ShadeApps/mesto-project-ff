65453881e954cbafc99c87afe22b55c368879e73
Sergey Grishchev <sergey@shadeapps.com>
2025-06-01 01:18:46 +0100

    Support CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -27,6 +28,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 };
